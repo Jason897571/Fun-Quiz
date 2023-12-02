a16038c18f628cbcbab273fee810953151b7f0c5
Jason <king.gs1314@outlook.com>
2023-12-01 22:01:54 -0500

    complete the clock function

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,8 +1,26 @@
-let time = document.getElementById("timer_number");
-let total_score = document.getElementById("total score");
+let time_element = document.getElementById("timer_number");
+let total_score_element = document.getElementById("total score");
+
+var time_left = 10;
+
+function start_timer(){
+    var timer = setInterval(function(){
+        time_left--;
+        time_element.innerHTML = time_left;
+        if(time_left < 0){
+            clearInterval(timer);
+            jump_to_finish_page();
+        }
+    }, 1000);
+}
 
 
 
 function jump_to_finish_page(){
-    
+    window.location.href = "finish_page.html";
 }
+
+
+start_timer()
+
+
