624aa8ff8a5100459137d47f264b1a23b8ed472a
Jason <king.gs1314@outlook.com>
2023-12-02 16:32:16 -0500

    update question generator js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,76 +1,83 @@
-let questions_holder = document.querySelector(".question_holder")
-
+let questions_text = document.querySelector(".question_text")
+let option_1 = document.querySelector(".option_1")
+let option_2 = document.querySelector(".option_2")
+let option_3 = document.querySelector(".option_3")
+let option_4 = document.querySelector(".option_4")
+let question_options_holder = document.querySelector(".question_options_holder")
+var question_index = 1;
 
 function create_question(question_text,options,correct_option) {
-    /* Create question element */
-    var question_text_element = document.createElement("h1");
-    question_text_element.innerText = question_text;
-    question_text_element.className = "question_text";
-    questions_holder.appendChild(question_text_element);
+    /* assign value to question text */
+    questions_text.textContent = question_text;
 
-    /* create quesiton options holder */
-
-    var question_options_holder = document.createElement("ul");
-    question_options_holder.className = "question_options_holder";
-    questions_holder.appendChild(question_options_holder);
-
-    /* create question option */
-
-    var question_option_element = document.createElement("li");
-    question_option_element.className = "question_option";
-    question_option_element.innerText = options[0];
-    question_options_holder.appendChild(question_option_element);
-
-    var question_option_element = document.createElement("li");
-    question_option_element.className = "question_option";
-    question_option_element.innerText = options[1];
-    question_options_holder.appendChild(question_option_element);
-
-    var question_option_element = document.createElement("li");
-    question_option_element.className = "question_option";
-    question_option_element.innerText = options[2];
-    question_options_holder.appendChild(question_option_element);
-
-    var question_option_element = document.createElement("li");
-    question_option_element.className = "question_option";
-    question_option_element.innerText = options[3];
-    question_options_holder.appendChild(question_option_element);
+    /* assign value to options text */
+    option_1.textContent = options[0];
+    option_2.textContent = options[1]; 
+    option_3.textContent = options[2]; 
+    option_4.textContent = options[3];
 
 }
 
+/* Json reader and store data into localStorage */
+var get_json_data = function(){
+    const quesiton_json_url = "./assets/json/question.json"
 
-const quesiton_json_url = "./assets/json/question.json"
+    fetch(quesiton_json_url)
+    .then(response => response.json())
+    .then(data => {
 
-fetch(quesiton_json_url)
-  .then(response => response.json())
-  .then(data => {
-
-    if(localStorage.getItem("question_data") == null){
-        // Use the data from the JSON file
-        localStorage.setItem("question_data", JSON.stringify(data));
-    }else{
-        return true;
-    }
-
-    
-  })
+        if(localStorage.getItem("question_data") == null){
+            // Use the data from the JSON file
+            localStorage.setItem("question_data", JSON.stringify(data));
+            
+        }else{
+            return false;
+        }
+    })
+}
 
 
-var questions_data = localStorage.getItem("question_data");
-questions_data = JSON.parse(questions_data);
+var display_question = function(){
+    var questions_data = localStorage.getItem("question_data");
+    questions_data = JSON.parse(questions_data);
 
-for(var i = 0; i < questions_data["multiple_question"].length; i++){
-
-    var single_question = questions_data["multiple_question"][i]
-
+    var single_question = questions_data["multiple_question"][question_index]
     var question_text = single_question["question"];
     var options = single_question["options"];
-    var correct_answer = single_question["correct_answer"];
+    var correct_answer = single_question["correctAnswer"];
     
-    create_question(question_text, options, correct_answer)
+    
+    create_question(question_text, options, correct_answer);
 
+    question_options_holder.addEventListener("click", function handle_option_click(event){
+        question_options_holder.removeEventListener("click", handle_option_click);
+
+        if(event.target.textContent == correct_answer){
+            console.log("correct answer")}
+        else{
+            console.log("wrong answer")
+        }
+    })
+
+    question_index ++;
+
+    if(question_index < questions_data["multiple_question"].length){
+        display_question();
+
+    }
+    else{
+        console.log("end of questions")
+    }
     
+
 }
+
+get_json_data();
+
+
+display_question()
+
+
 
 
 
